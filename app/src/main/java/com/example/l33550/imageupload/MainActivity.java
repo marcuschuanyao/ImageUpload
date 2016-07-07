d2340940cc44f6f8ddefe7c2ece1e130a7bfd123
paperbag95 <chuanyao1995@hotmail.com>
2016-07-07 15:05:11 +0800

    Update the upload file

--- app/src/main/java/com/example/l33550/imageupload/MainActivity.java
+++ app/src/main/java/com/example/l33550/imageupload/MainActivity.java
@@ -5,13 +5,23 @@ import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Debug;
 import android.provider.MediaStore;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferService;
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
 
@@ -19,10 +29,12 @@ import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int PICK_IMAGE_REQUEST = 1;
 
-    FloatingActionMenu materialDesignFAM;
-    FloatingActionButton floatingActionButton2, floatingActionButton3;
+
+    File fileToUpload = new File("/storage/emulated/0/Download/7.png");
+    File fileToDownload = new File("/storage/sdcard0/Pictures/MY");
+    AmazonS3 s3;
+    TransferUtility transferUtility;
 
 
     @Override
@@ -33,29 +45,102 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-        materialDesignFAM = (FloatingActionMenu) findViewById(R.id.material_design_android_floating_action_menu);
-        floatingActionButton2 = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item2);
-        floatingActionButton3 = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item3);
+        // callback method to call credentialsProvider method.
+        credentialsProvider();
+
+        // callback method to call the setTransferUtility method
+        setTransferUtility();
 
 
-        floatingActionButton2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                //TODO something when floating action menu second item clicked
+
+    }
 
 
+    /**
+     *  Create a AmazonS3Client constructor and pass the credentialsProvider.
+     * @param credentialsProvider
+     */
+    public void setAmazonS3Client(CognitoCachingCredentialsProvider credentialsProvider){
+
+        // Create an S3 client
+        s3 = new AmazonS3Client(credentialsProvider);
+
+        // Set the region of your S3 bucket
+        s3.setRegion(Region.getRegion(Regions.US_EAST_1));
+
+    }
+
+    public void setTransferUtility(){
+
+        transferUtility = new TransferUtility(s3, getApplicationContext());
+    }
+
+    public void credentialsProvider(){
+
+        // Initialize the Amazon Cognito credentials provider
+        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(),
+                "us-east-1:b8ab0f4a-865b-45d0-a9df-16cf02f25173", // Identity Pool ID
+                Regions.US_EAST_1 // Region
+        );
+
+        setAmazonS3Client(credentialsProvider);
+    }
+
+
+    public void setFileToUpload(View v){
+        Log.d("hello world","test");
+
+        TransferObserver transferObserver = transferUtility.upload(
+                "imageuploadnypsit",     /* The bucket to upload to */
+                "7.png",       /* The key for the uploaded object */
+                fileToUpload       /* The file where the data to upload exists */
+        );
+        transferObserverListener(transferObserver);
+    }
+
+
+    public void setFileToDownload(View v){
+
+        TransferObserver transferObserver = transferUtility.download(
+                "imageuploadnypsit",     /* The bucket to download from */
+                "MY",    /* The key for the object to download */
+                fileToDownload        /* The file to download the object to */
+        );
+
+        transferObserverListener(transferObserver);
+
+    }
+
+
+    /**
+     * This is listener method of the TransferObserver
+     * Within this listener method, we get the status of uploading and downloading the file,
+     * it displays percentage part of the  file to be uploaded or downloaded to S3
+     * It also displays an error, when there is problem to upload and download file to S3.
+     * @param transferObserver
+     */
+
+    public void transferObserverListener(TransferObserver transferObserver){
+
+        transferObserver.setTransferListener(new TransferListener(){
+
+            @Override
+            public void onStateChanged(int id, TransferState state) {
+                Log.d("statechange", state+"");
             }
-        });
-        floatingActionButton3.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                //TODO something when floating action menu third item clicked
-                Intent intent = new Intent();
-                // Show only images, no videos or anything else
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                // Always show the chooser (if there are multiple options available)
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
 
+            @Override
+            public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+                int percentage = (int) (bytesCurrent/bytesTotal * 100);
+                Log.d("percentage",percentage +"");
             }
+
+            @Override
+            public void onError(int id, Exception ex) {
+                Log.d("error","error");
+            }
+
         });
     }
 
