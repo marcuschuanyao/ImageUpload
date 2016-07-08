2c04d3314d770815111aeb752a49323ae49f5457
paperbag95 <chuanyao1995@hotmail.com>
2016-07-08 11:31:58 +0800

    Upload the image and add the toast.

--- app/src/main/java/com/example/l33550/imageupload/MainActivity.java
+++ app/src/main/java/com/example/l33550/imageupload/MainActivity.java
@@ -2,16 +2,21 @@ package com.example.l33550.imageupload;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Debug;
 import android.provider.MediaStore;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferService;
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
@@ -26,16 +31,23 @@ import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    ImageView imageView;
 
-    File fileToUpload = new File("/storage/emulated/0/Download/7.png");
+    File fileToUpload;
     File fileToDownload = new File("/storage/sdcard0/Pictures/MY");
     AmazonS3 s3;
     TransferUtility transferUtility;
 
+    private CoordinatorLayout coordinatorLayout;
+
+    private static final int SELECT_PHOTO = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +56,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Instantiate the iamge view
+        imageView = (ImageView)findViewById(R.id.imageView);
+
+        // Instantiate coordinator layout
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id
+                .coordinatorLayout);
 
         // callback method to call credentialsProvider method.
         credentialsProvider();
@@ -88,17 +106,84 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void setFileToUpload(View v){
-        Log.d("hello world","test");
+    public void browseImageFile(View v){
+
+        /*Log.d("hello world","test");
+
+        TransferObserver transferObserver = transferUtility.upload(
+                "imageuploadnypsit",      The bucket to upload to
+                "7.png",       /* The key for the uploaded object
+                fileToUpload       /* The file where the data to upload exists
+        );
+        transferObserverListener(transferObserver);*/
+
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+
+    }
+
+    public void uploadImage(File uploadFile) {
+
+        // Generate random file string to prevent
+        String uuid = UUID.randomUUID().toString();
 
         TransferObserver transferObserver = transferUtility.upload(
                 "imageuploadnypsit",     /* The bucket to upload to */
-                "7.png",       /* The key for the uploaded object */
-                fileToUpload       /* The file where the data to upload exists */
+                uuid+".jpg",       /* The key for the uploaded object */
+                uploadFile       /* The file where the data to upload exists */
         );
         transferObserverListener(transferObserver);
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch(requestCode) {
+            case SELECT_PHOTO:
+                if(resultCode == RESULT_OK){
+
+                    // Get the selected image file
+                    Uri selectedImage = imageReturnedIntent.getData();
+
+                    // Get the file URL
+                    fileToUpload = new File(getRealPathFromURI(selectedImage));
+
+                    InputStream imageStream = null;
+                    try {
+                        imageStream = getContentResolver().openInputStream(selectedImage);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    Bitmap yourSelectedImage = BitmapFactory.decodeStream(imageStream);
+
+                    imageView.setImageBitmap(yourSelectedImage);
+
+                    // Upload image into server
+                    uploadImage(fileToUpload);
+
+                    Snackbar snackbar = Snackbar
+                            .make(coordinatorLayout, "Image has been uploaded.", Snackbar.LENGTH_SHORT);
+
+                    snackbar.show();
+
+
+                }
+        }
+
+
+
+    }
+
+
+    public String getRealPathFromURI(Uri uri) {
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+        return cursor.getString(idx);
+    }
 
     public void setFileToDownload(View v){
 
@@ -139,6 +224,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onError(int id, Exception ex) {
                 Log.d("error","error");
+
+                Snackbar snackbar = Snackbar
+                        .make(coordinatorLayout, "Please allow the app's permission to read the file.", Snackbar.LENGTH_SHORT);
+
+                snackbar.show();
             }
 
         });
